df957950f0ca495ddc6166792127958a5a319cac
Ron Levy <ronlevy12000@gmail.com>
2021-01-19 16:44:10 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, send_from_directory
+from twilio.twiml.messaging_response import MessagingResponse
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
+
+    # Create reply
+    resp = MessagingResponse()
+    resp.message("You said: {}, which is very interesting! So you're from ITC, is your name Yoni?".format(msg))
+
+    return str(resp)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
